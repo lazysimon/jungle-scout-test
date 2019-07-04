e262064dba3ee3703ab62cf6c7d9d4fece9b8dbe
Nicole <nhswong@gmail.comm>
2019-07-04 11:15:57 -0700

    scrape and save data with ASIN Input

--- app/src/componets/ASINProducts/ASINInput.js
+++ app/src/componets/ASINProducts/ASINInput.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { Form, Button, Container } from 'semantic-ui-react'
+import axios from 'axios'
 
 export default class ASINInput extends Component {
   constructor(props) {
@@ -7,7 +8,7 @@ export default class ASINInput extends Component {
 
 
     this.state = {
-      isLoading: false,
+      asin: ''
     }
   }
 
@@ -15,16 +16,38 @@ export default class ASINInput extends Component {
 
   }
 
-  onSubmitInput(e) {
-    this.props.input()
+  /**
+   * scrape data given asin and save it to db
+   */
+  scrapeAndSave() {
+    const { asin } = this.state
+    
+    this.setState({isLoading: true})
+    axios.post('http://localhost:8080/api/product/scrape/add', 
+      {
+        asin: asin
+      })
+      .then((product) => {
+        console.log('-data', product.data)
+        this.setState({isLoading: false})
+        this.forceUpdate()
+      })
+      .catch((error) => {
+
+      })
+
   }
 
   render() {
+    const { isLoading } = this.state
     return (
-      <Container className='asin-input-container'>
-        <Form>
-        <Form.Input className='asin-input' placeholder='ASIN' />
-          <Button className='asin-submit-btn' type='submit'>Submit</Button>
+      <Container textAlign='left' className='asin-input-container'>
+        <Form onSubmit={() => this.scrapeAndSave()}>
+        <h4>Get Product Data by ASIN:</h4>
+        <Form.Input className='asin-input' disabled={isLoading} placeholder='ASIN' onChange={(event) => {
+          console.log(event.target.value)
+          this.setState({asin: event.target.value})}}/>
+          <Button className='asin-submit-btn' type='submit' loading={isLoading}>Submit</Button>
         </Form>
       </Container>
     )
