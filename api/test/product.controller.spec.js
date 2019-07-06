8c65c80efb7856be9272cac8d4e068ff2fc2513c
Nicole <nhswong@gmail.comm>
2019-07-05 19:14:03 -0700

    create tests

--- api/test/product.controller.spec.js
+++ api/test/product.controller.spec.js
@@ -1,14 +1,11 @@
-var productController =  require('../controllers/product.controller');
-
 const chai = require('chai');
 let chaiHttp = require('chai-http');
 chai.use(chaiHttp);
-let Product = require('../models/product.model');
-
-
-const request = require('supertest');
-
-const server = 'http://localhost:8080';
+const Product = require('../models/product.model');
+const supertest = require('supertest');
+const server =  require('../server');
+const request = supertest(server);
+const should = chai.should();
 
 describe('Products', function() {
   const productsArr = [
@@ -26,21 +23,43 @@ describe('Products', function() {
     }
   ];
 
-  // beforeEach((done) => {
-  //   done();
-  // })
+  after(() => {
+    console.log('Done executing all tests... Dropping all collections');
+    Product.db.dropCollection('products');
+    server.close((err) => console.log('Unable to close the server : ', err));
+  })
 
   describe('POST /products/add', () => {
     it('should add products', (done) => {
-      request(server)
+      request
         .post('/api/product/add')
         .send(productsArr[0])
         .expect(200)
         .expect((res) => {
-          res.body.asin.should.equal('1234')
-          res.body.productDimensions.should.equal('6.5 x 4.3 x 2.1 inches')
-          res.body.category.should.equal('morty')
-          res.body.rank.should.equal('#1 in dogs')
+          res.body.product.asin.should.equal('1234')
+          res.body.product.productDimensions.should.equal('6.5 x 4.3 x 2.1 inches')
+          res.body.product.category.should.equal('morty')
+          res.body.product.rank.should.equal('#1 in dogs')
+        })
+        .end((err, res) => {
+          if(err) throw err;
+          done();
+        })
+    })
+  })
+
+  describe('POST /products/scrape/add', () => {
+    it('should scrape and save product', (done) => {
+      request
+        .post('/api/product/scrape/add')
+        .send({asin: 'B003AIM52A'})
+        .set('Accept', 'application/json')
+        .expect(200)
+        .expect((res) => {
+          res.body.should.be.a('object')
+          res.body.product.asin.should.equal('B003AIM52A')
+          res.body.product.productDimensions.should.equal('7.5 x 8 x 1.4 inches')
+          res.body.product.category.should.equal('Toys & Games')
         })
         .end((err, res) => {
           if(err) throw err;
@@ -51,35 +70,21 @@ describe('Products', function() {
 
   describe('GET /products/', () => {
     it('should get all products', (done) => {
-      // create products first
-      // get products
-      // check status equals 200
-      // check products length 
-      // check property keys
-
-      request(server)
+      request
         .get('/api/product/')
         .set('Accept', 'application/json')
         .expect('Content-Type', /json/)
         .expect(200)
         .expect((res) => {
-          console.log(res.body)
           res.body.should.be.a('array')
           res.body.length.should.be.equal(2)
-          res.body[0].should.have.all.keys('asin', 'category', 'productDimensions', 'rank')
+          res.body[0].should.contain.keys('asin', 'category', 'productDimensions', 'rank')
         })
         .end((err, res) => {
           if(err) throw err;
-
           done();
         })
     })
   })
 
-  describe('POST /products/scrape/add', () => {
-    it('should scrape and save product', (done) => {
-      request(server)
-        .get('/api/product/scrape/add')
-    })
-  })
 })
